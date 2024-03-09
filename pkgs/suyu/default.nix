5e72064dd4ac1b0050a487ab1921fc7fd31ef43f
pillowtrucker <githubino@janitor.chat>
2024-03-09 12:09:25 +0100

    NUR/cachix don't like scopes it seems

--- pkgs/suyu/default.nix
+++ pkgs/suyu/default.nix
@@ -1,12 +1,8 @@
 { qt6Packages, makeScopeWithSplicing', generateSplicesForMkScope }:
-
-makeScopeWithSplicing' {
-  otherSplices = generateSplicesForMkScope "suyuPackages";
-  f = self: qt6Packages // {
-    compat-list = self.callPackage ./compat-list.nix {};
-    nx_tzdb = self.callPackage ./nx_tzdb.nix {};
-
-    dev = self.callPackage ./dev.nix {};
+{
+    compat-list = qt6Packages.callPackage ./compat-list.nix {};
+    nx_tzdb = qt6Packages.callPackage ./nx_tzdb.nix {};
+    dev = qt6Packages.callPackage ./dev.nix {};
 #    early-access = self.callPackage ./early-access {};
-  };
+
 }
--- pkgs/yuzu/default.nix
+++ pkgs/yuzu/default.nix
@@ -1,13 +1,9 @@
 { qt6Packages, makeScopeWithSplicing', generateSplicesForMkScope }:
+{
+    compat-list = qt6Packages.callPackage ./compat-list.nix {};
+    nx_tzdb = qt6Packages.callPackage ./nx_tzdb.nix {};
 
-makeScopeWithSplicing' {
-  otherSplices = generateSplicesForMkScope "yuzuPackages";
-  f = self: qt6Packages // {
-    compat-list = self.callPackage ./compat-list.nix {};
-    nx_tzdb = self.callPackage ./nx_tzdb.nix {};
+    mainline = qt6Packages.callPackage ./mainline.nix {};
+    early-access = qt6Packages.callPackage ./early-access {};
 
-    mainline = self.callPackage ./mainline.nix {};
-    early-access = self.callPackage ./early-access {};
-    
-  };
 }
